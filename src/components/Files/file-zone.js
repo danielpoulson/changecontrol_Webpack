489e8c1a106176692a3f9639748ae5c36ec838f8
danielpoulson <danielpoulson@hotmail.com.au>
2016-04-10 10:44:58 +1000

    eslint / refactor

--- src/components/Files/file-zone.js
+++ src/components/Files/file-zone.js
@@ -1,52 +1,57 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Dropzone from 'react-dropzone';
 import toastr from 'toastr';
 import Request from 'superagent';
 
 class FileZone extends React.Component {
-    onDrop = (files) => {
+  // TODO: MED 2 Change function of the filezone drop function to multiply files
+  onDrop = (files) => {
+    var file = files[0];
+    var sourceId = this.props.sourceId;
+    var addFile = this.props.addFile;
+    var req = Request.post('/server/upload');
 
-        var file = files[0];
-        var sourceId = this.props.sourceId;
-        var addFile = this.props.addFile;
-        var req = Request.post('/server/upload');
+    files.forEach((file) => {
 
-        files.forEach((file)=> {
+      const myRe = /C{2}\d{6}\s[-]\s/;
+      var _fileName = file.name;
+      var _newFileName = '';
+      var myArray = myRe.exec(_fileName);
 
-          const myRe = /C{2}\d{6}\s[-]\s/;
-          var _fileName = file.name;
-          var _newFileName  = '';
-          var myArray = myRe.exec(_fileName);
+      if (myArray) {
+        _newFileName = _fileName;
+      } else {
+        _newFileName = `${sourceId} - ${file.name}`;
+      }
 
-          if(myArray) {
-              _newFileName =  _fileName;
-          } else {
-              _newFileName =  sourceId + " - " + file.name;
-          }
+      req.field('sourceId', sourceId);
+      req.field('dpUser', this.props.user.username);
+      req.field('docName', file.name);
+      req.attach(_newFileName, file, file.name);
+    });
 
-            req.field('sourceId', sourceId );
-            req.field('dpUser', this.props.user.username );
-            req.field('docName', file.name );
-            req.attach(_newFileName, file, file.name);
-        });
+    req.end((err, res) => addFile(res.body));
 
-        req.end(function(err, res){
-          addFile(res.body);
-        });
+    toastr.success('File has been uploaded', 'File Upload', { timeOut: 1000 });
+  };
 
-        toastr.success("File has been uploaded", "File Upload", {timeOut:1000});
-    };
+  render() {
 
-    render() {
+    return (
+        <div className="margin-20-top">
+          <Dropzone className="drop-zone" multiple={false} onDrop={this.onDrop}>
+            <div>File Drop or click to select single file (upload).</div>
+          </Dropzone>
+        </div>
+    );
+  }
+}
 
-      return (
-          <div className="margin-20-top">
-            <Dropzone className="drop-zone" multiple={false} onDrop={this.onDrop}>
-              <div>File Drop or click to select single file (upload).</div>
-            </Dropzone>
-          </div>
-      );
-    }
+FileZone.propTypes = {
+  sourceId: PropTypes.string,
+  addFile: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+
 };
 
 export default FileZone;
