79f27ebbc2071f357e398a3b4bfd6691543392d3
danielpoulson <danielpoulson@hotmail.com.au>
2016-09-18 17:09:45 +1000

    Adding create app

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { Provider } from 'react-redux';
+import { Router, Redirect, browserHistory } from 'react-router';
+// import { createHashHistory } from 'history';
+import configureStore from './store/configureStore';
+import routes from './routes';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Redirect from="/" to="home" />
+      {routes}
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
